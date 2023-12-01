0cb84e111ee6f536e2d0ab6137df6712a65e16f1
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-12-01 18:11:42 +0100

    add a comment form an a comments list

--- src/pages/api/venues/[id]/index.js
+++ src/pages/api/venues/[id]/index.js
@@ -10,19 +10,21 @@ export default async function handler(request, response) {
   }
 
   await dbConnect();
+
   if (request.method === "GET") {
     const venue = await Venue.findById(id);
     if (!venue) {
       return response.status(404).json({ status: "Not found" });
     }
-    response.status(200).json(venue);
+    const comments = await Comment.find({ venueID: id });
+    response.status(200).json({ venue, comments });
   }
 
   if (request.method === "POST") {
     try {
       const comment = request.body;
       await Comment.create(comment);
-      // console.log(comment);
+      console.log(comment);
       return response.status(201).json(comment);
     } catch (error) {
       return response.status(400).json({ error: error.message });
