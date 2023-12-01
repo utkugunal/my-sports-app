4c6bbd4b10724bea9e699daad704f228ed3908e0
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-12-01 16:53:32 +0100

    branch update

--- components/Comments/Comments.js
+++ components/Comments/Comments.js
@@ -21,6 +21,7 @@ export default function Comments() {
     if (response.ok) {
       mutate();
     }
+    e.target.reset();
   }
 
   return (
--- db/models/Comment.js
+++ db/models/Comment.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const commentSchema = new Schema({
   username: { type: String, required: true },
   comment: { type: String, required: true },
-  venue: { type: String, required: true },
+  venue: { type: Schema.Types.ObjectId, ref: "Venue" },
 });
 
 const Comment =
