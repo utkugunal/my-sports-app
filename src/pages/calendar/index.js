d3a3c51733510b56b5858d28db5b58086dd149d6
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-12-05 11:09:58 +0100

    add a formattedDateStr to the calendar code

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -1,9 +1,7 @@
+import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import React from "react";
 import styled from "styled-components";
-
-const events = [{ title: "Meeting", start: new Date() }];
 
 const Container = styled.div`
   height: 50%;
@@ -12,6 +10,24 @@ const Container = styled.div`
 `;
 
 export default function DemoApp() {
+  const [formattedDate, setFormattedDate] = useState("");
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Month is zero-based
+    const day = String(currentDate.getDate()).padStart(2, "0");
+
+    const formattedDateStr = `${year}-${month}-${day}`;
+    setFormattedDate(formattedDateStr);
+  }, []); // Empty dependency array to run the effect only once when the component mounts
+
+  const events = [
+    { title: "Meeting", start: formattedDate },
+    { title: "Fitness", start: "2023-12-08" },
+  ];
+
+  console.log("currentDateStr: ", formattedDate);
   return (
     <Container>
       <h1>My Sports Calendar</h1>
